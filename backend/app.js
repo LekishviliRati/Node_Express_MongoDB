358271cf930927e211dc3d7360f151ed429a36b8
LekishviliRati <lekishvili.rati@gmail.com>
2022-07-27 18:35:23 +0200

    try to connect to Mongo DB but site is not accessible at the moment

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+
 app.use(express.json());
 
 app.use((req, res, next) => {
